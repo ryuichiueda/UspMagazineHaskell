fc21b6cc60db503eb99bf6ed776b31793d21afa1
Ryuichi UEDA <ueda@test.usptomo.com>
2014-06-23 14:31:08 +0900

    Rewrite of 201409

--- Study1_Q3/q1_3_3.hs
+++ Study1_Q3/q1_3_3.hs
@@ -6,7 +6,5 @@ ls :: String -> IO [FilePath]
 ls dir = getDirectoryContents dir >>= return . filter (`notElem` [".",".."])
 
 digdir :: FilePath -> IO [FilePath]
-digdir dir = ls dir >>= mapM (digdir' dir) >>= return . concat
-
-digdir' :: FilePath -> FilePath -> IO [FilePath]
-digdir' = undefined
+digdir dir = ls dir 
+             >>= mapM (\x -> return $ dir ++ "/" ++ x) 
--- Study1_Q3/q1_3_4.hs
+++ Study1_Q3/q1_3_4.hs
@@ -1,14 +1,16 @@
 import System.Directory
 
-main = digdir "/etc" >>= putStrLn . unlines
+main = digdir "/etc" >>= putStr . unlines
 
 ls :: String -> IO [FilePath]
 ls dir = getDirectoryContents dir >>= return . filter (`notElem` [".",".."])
 
 digdir :: FilePath -> IO [FilePath]
-digdir dir = ls dir >>= mapM (digdir' dir) >>= return . concat
+digdir dir = ls dir 
+             >>= mapM (\x -> return $ dir ++ "/" ++ x) 
+             >>= mapM digdir'
+             >>= return . concat
 
-digdir' :: FilePath -> FilePath -> IO [FilePath]
-digdir' d f = do let p = d ++ "/" ++ f
-                 b <- doesDirectoryExist p
-                 if b then digdir p else return [p]
+digdir' :: FilePath -> IO [FilePath]
+digdir' path = do b <- doesDirectoryExist path
+                  if b then digdir path else return [path]
